c77eae45a2464627345a92f43cc08e52676aed4d
Christian Guaman <christian92m5@gmail.com>
2023-01-04 21:55:50 +0100

    Example Request Params in Post

--- src/main/java/com/course/controller/CategoriasController.java
+++ src/main/java/com/course/controller/CategoriasController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping(value = "/categorias")
@@ -21,7 +22,11 @@ public class CategoriasController {
     }
 
     @PostMapping("/save")
-    public String guardar() {
+    public String guardar(@RequestParam("nombre") String nombre, @RequestParam("descripcion") String descripcion) {
+        System.out.println("nombre: "+ nombre);
+        System.out.println("descripcion: "+ descripcion);
+
+
         return "categorias/listCategorias";
     }
 }
