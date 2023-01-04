f9df7df2a09394ba7f402a0ef96d88bd98f6e176
Christian Guaman <christian92m5@gmail.com>
2023-01-04 21:26:06 +0100

    Add refs to table

--- src/main/java/com/course/controller/VacantesController.java
+++ src/main/java/com/course/controller/VacantesController.java
@@ -1,2 +1,19 @@
-package com.course.controller;public class VacantesController {
+package com.course.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vacantes")
+public class VacantesController {
+
+    @GetMapping("/view/{id}")
+    public String verDetalle(@PathVariable("id") int idVacante, Model model){
+        System.out.println("idVacante: "+ idVacante);
+        model.addAttribute("idVacante", idVacante);
+        return "vacantes/detalle";
+    }
 }
