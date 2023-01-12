0018fc91abda93dcf89b5956265423e651d594f0
Christian Guaman <christian92m5@gmail.com>
2023-01-12 02:45:16 +0100

    Use BindingResult for errors

--- src/main/java/com/course/controller/VacantesController.java
+++ src/main/java/com/course/controller/VacantesController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,7 +35,14 @@ public class VacantesController {
     }
 
     @PostMapping("/save")
-    public String guardar(Vacante vacante){
+    public String guardar(Vacante vacante, BindingResult bindingResult ){
+        if(bindingResult.hasErrors()){
+            for(var error: bindingResult.getAllErrors()){
+                System.out.println("Ocurrio un error: "+ error.getDefaultMessage());
+
+            }
+            return "vacantes/formVacante";
+        }
         System.out.println("vacante in: "+ vacante);
 
         vacantesService.guardar(vacante);
