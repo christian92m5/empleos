6abfce1fbeb29255d90aa64ec234754d1faeb2be
Christian Guaman <christian92m5@gmail.com>
2023-01-12 21:36:12 +0100

    Uso de Redirect

--- src/main/java/com/course/controller/VacantesController.java
+++ src/main/java/com/course/controller/VacantesController.java
@@ -49,7 +49,7 @@ public class VacantesController {
 
         vacantesService.guardar(vacante);
 
-        return "vacantes/listVacantes";
+        return "redirect:/vacantes/index";
     }
 
     @GetMapping("/delete")
