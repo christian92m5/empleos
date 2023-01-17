511a3f9e98871d5c892a5451525dceeb4b04b21b
Christian Guaman <christian92m5@gmail.com>
2023-01-17 19:15:25 +0100

    Añadimos ejecucion por comando de linea con CommandLineRunner

--- src/main/java/com/course/EmpleosApplication.java
+++ src/main/java/com/course/EmpleosApplication.java
@@ -1,13 +1,22 @@
 package com.course;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmpleosApplication {
+public class EmpleosApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmpleosApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		guardar();
+	}
+	private void guardar(){
+		System.out.println("Guardar vacantes");
+	}
 }
