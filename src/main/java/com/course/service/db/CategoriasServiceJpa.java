4b24cbd74cf5e72bd68162f4c2f970ee191fb4a4
Christian Guaman <christian92m5@gmail.com>
2023-01-19 20:25:39 +0100

    Implementacion de CategoriasServiceJpa

---
+++ src/main/java/com/course/service/db/CategoriasServiceJpa.java
@@ -0,0 +1,31 @@
+package com.course.service.db;
+
+import com.course.model.Categoria;
+import com.course.repository.CategoriasRepository;
+import com.course.service.ICategoriasService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoriasServiceJpa implements ICategoriasService {
+
+    @Autowired
+    private CategoriasRepository categoriasRepository;
+
+    @Override
+    public void guardar(Categoria categoria) {
+        categoriasRepository.save(categoria);
+    }
+
+    @Override
+    public List<Categoria> buscarTodas() {
+        return categoriasRepository.findAll();
+    }
+
+    @Override
+    public Categoria buscarPorId(Integer idCategoria) {
+        return categoriasRepository.findById(idCategoria).orElse(null);
+    }
+}
